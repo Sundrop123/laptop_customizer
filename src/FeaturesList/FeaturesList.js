37d004bc94fa67b5740b70bece85b0816ea32e3e
Sundrop123 <47043481+Sundrop123@users.noreply.github.com>
2019-04-25 12:00:08 -0400

    Update FeaturesList.js

--- src/FeaturesList/FeaturesList.js
+++ src/FeaturesList/FeaturesList.js
@@ -1,1 +1,32 @@
+import React, { Component } from 'react';
+import FeaturesListItem from './FeaturesListItem'
+import './FeaturesList.css'
 
+class FeaturesList extends Component {
+  
+
+  render() {
+   
+    const features = Object.keys(this.props.features).map((value) => {
+       return(
+       <div className="feature" key={value}>
+        <div className="feature__name">{value}</div>
+        <ul className="feature__list">
+            <FeaturesListItem  
+                      category = {value}
+                      items = {this.props.items}
+                      features = {this.props.features}
+                     onClickUpdate = {this.props.onClickUpdate}
+                     />
+            </ul>
+            </div>
+        
+          )
+
+      });
+      return <div> {features}</div>
+ }
+}    
+
+
+export default FeaturesList;
