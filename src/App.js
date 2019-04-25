adb89426fd6d6147ddac8fd17fba5aca1112775b
Sundrop123 <47043481+Sundrop123@users.noreply.github.com>
2019-04-25 12:01:31 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
+import SummaryList from './Summary/SummaryList';
+import FeaturesList from './FeaturesList/FeaturesList';
 
 class App extends Component {
   constructor(props){
-    super(props);
+    super(props)
     this.state = {
       selected: {
-        Processor: {
+        "Processor": {
             name: '17th Generation Intel Core HB (7 Core with donut spare)',
             cost: 700
           },
@@ -18,7 +20,7 @@ class App extends Component {
             name: 'Toyota Corolla 1.5v',
             cost: 1150.98
           },
-        Display: {
+        "Display": {
             name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
             cost: 1500
           }
@@ -26,7 +28,7 @@ class App extends Component {
     }
   }
 
-  updateFeature(feature, newValue) {
+  updateFeature = (feature,newValue)=> {
     const selected = Object.assign({}, this.state.selected);
     selected[feature] = newValue;
     this.setState({
@@ -35,44 +37,7 @@ class App extends Component {
   }
 
   render() {
-    const summary = Object.keys(this.state.selected)
-          .map(key => <div className="summary__option" key={key}>
-            <div className="summary__option__label">{key}  </div>
-            <div className="summary__option__value">{this.state.selected[key].name}</div>
-            <div className="summary__option__cost">
-              { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                  .format(this.state.selected[key].cost) }
-            </div>
-        </div>)
-
-    const total = Object.keys(this.state.selected)
-          .reduce((acc, curr) => acc + this.state.selected[curr].cost, 0);    
-
-
-    const features = Object.keys(this.props.features)
-          .map(key => {
-            const options = this.props.features[key].map((item, index) => {
-              const selectedClass = item.name === this.state.selected[key].name ? 'feature__selected' : '';
-              const featureClass = 'feature__option ' + selectedClass;
-              return <li key={index} className="feature__item">
-                <div className={featureClass}
-                  
-                  onClick={e => this.updateFeature(key, item)}>
-                    { item.name }
-                    ({ new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                      .format(item.cost) })
-                </div>
-              </li>
-            });
-
-            return <div className="feature" key={key}>
-              <div className="feature__name">{key}</div>
-              <ul className="feature__list">
-                { options }
-              </ul>
-            </div>
-          });      
-
+    console.log(this.props.features)
     return (
       <div className="App">
         <header>
@@ -82,24 +47,20 @@ class App extends Component {
         </header>      
         <main>
           <section className="main__form">
-            <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
-            { features }
-          </section>
-          <section className="main__summary">
-            <h3>NEW GREENLEAF 2018</h3>
-            {summary}
-            <div className="summary__total">
-              <div className="summary__total__label">Your Price: </div>
-              <div className="summary__total__value">
-              { new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD'})
-                  .format(total) }
-              </div>
-            </div>
-          </section>
+          <h3>TECH SPECS AND CUSTOMIZATIONS</h3>
+        <FeaturesList items ={this.state.selected}
+                      features={this.props.features}
+                      onClickUpdate = {(feature,newValue)=>this.updateFeature(feature,newValue)} />
+                      </section>
+                      <section className="main__summary">
+                      <h3>NEW GREENLEAF 2018</h3>
+        <SummaryList items = {this.state.selected}
+                 />
+                 </section>
         </main>
       </div>
     );
   }
 }
 
-export default App;  
+export default App;
